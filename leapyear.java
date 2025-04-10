11cd6d06b6685141a0c9357cc66c4c38fd989bbe
Keerthana Govindarajan <keerthanagovindrj@gmail.com>
2025-04-10 19:27:41 +0530

    Update leapyear.java

--- leapyear.java
+++ leapyear.java
@@ -5,7 +5,7 @@ public class leapyear {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a year:");
         int year = input.nextInt();
-        if (year % 4 == 0) {
+        if (((year % 4 == 0)&&(year% 100!= 0)) ||(year % 400 == 0)) {
             System.out.println(+year+" is a leap year");
         }
         else{
